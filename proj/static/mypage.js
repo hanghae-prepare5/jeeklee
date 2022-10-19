eb4c231214f80c8d8bb18ee89b146677545faf65
JeekLee <j3kkun@gmail.com>
2022-10-19 13:07:40 +0900

    html 인스타 형식으로 변경

--- proj/static/mypage.js
+++ proj/static/mypage.js
@@ -7,21 +7,31 @@ const bmBtn = document.getElementById('BOOKMARK_BTN');
 // function activates when DOMtree is organized
 $(document).ready(function() {
   console.log('login.js called');
+  showmp();
 });
 
 // function to print recent log
 function showlog(){
-  console.log("function showlog() called")
+  console.log("function showlog() called");
 }
 
 // function to print recent log
 function showmp(){
-  console.log("function showmp() called")
+  console.log("function showmp() called");
+  mpBtn.style.borderTop = "1px solid #262626";
+  mpBtn.style.color = "#262626";
+  bmBtn.style.borderTop = "none";
+  bmBtn.style.color = "#DBDBDB";
 }
 
 // function to print bookmarks
 function showbm(){
-  console.log("function showbm() called")
+  console.log("function showbm() called");
+  bmBtn.style.borderTop = "1px solid #262626";
+  bmBtn.style.color = "#262626";
+  mpBtn.style.borderTop = "none";
+  mpBtn.style.color = "#DBDBDB";
+
 }
 
 // event Listeners
